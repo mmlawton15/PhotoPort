0e2860eb4a4e93594580beef44b55410865b850a
Mary Margaret <mmlawton15@gmail.com>
2022-03-19 10:44:34 -0400

    pushing to github as a safety check

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const {name, email, message} = formState;
     return (
         <section>
             <h1>Contact Me</h1>
             <form if="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" defaultValue={name} />
                 </div>
                 <div>
                     <label htmlFor="email">Email Address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" defaultValue={email} />
                 </div>              
                 <div>
                     <label htmlFor="message">Messgae:</label>
-                    <textarea name ="message" rows="5" />
+                    <textarea name ="message" rows="5" defaultValue={message}/>
                     <button type="submit">Submit</button>
                 </div>
             </form>

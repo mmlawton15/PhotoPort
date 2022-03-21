cd12d2830c84e7577d4f7f164bd25ba15d9934c5
Mary Margaret <mmlawton15@gmail.com>
2022-03-21 10:31:15 -0400

    refactoring with lesson code in preparation for deployment

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -125,17 +125,19 @@ const PhotoList = ({ category }) => {
   const currentPhotos = photos.filter(photo => photo.category === category);
 
   const toggleModal = (image, i) => {
-    setCurrentPhoto({...image, index: i});
-    setIsModalOpen(true);
-  }
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} /> }
+      {isModalOpen && (
+        <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`)}
+            src={require(`../../assets/small/${category}/${i}.jpg`).default}
             alt={image.name}
             className="img-thumbnail mx-1"
             onClick={() => toggleModal(image, i)}
